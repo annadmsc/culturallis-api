158bf100b1d254e69c3d66969f87c40c22f0f69e
annadmsc <ana.damasceno@germinare.org.br>
2023-10-06 17:57:21 -0300

    creating rote for login

--- src/main/java/com/example/demo/Usuarios/UsuariosRepository.java
+++ src/main/java/com/example/demo/Usuarios/UsuariosRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.Usuarios.UsuariosModel.Usuarios;
 
 public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
+
+    Usuarios findByEmail(String email);
 }
