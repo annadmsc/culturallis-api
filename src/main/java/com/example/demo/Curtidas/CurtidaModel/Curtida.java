c0f85f4b187aa6a87920f2694dcc6e6183ae638b
fidasgabriel <gabriel.fidalgo@germinare.org.br>
2023-10-26 21:01:09 -0300

    fixing getPostsByEmail query

--- src/main/java/com/example/demo/Curtidas/CurtidaModel/Curtida.java
+++ src/main/java/com/example/demo/Curtidas/CurtidaModel/Curtida.java
@@ -13,30 +13,41 @@ public class Curtida {
 
     private long fk_cul_usuarios_id;
 
-    private long fk_cul_posts_id;
+    @Column(name = "fk_cul_posts_id")
+    private long fkCulPostsId;
 
-    // private Date data_criacao;
+    @Column(name="data_criacao")
+     private Date dataCriacao;
 
-    // private Date data_mudanca;
+     private Date data_mudanca;
 
+     private Date data_desativacao;
     public Curtida() {
     }
 
-    public Curtida(long fk_cul_usuarios_id, long fk_cul_posts_id, Date data_criacao, Date data_mudanca) {
-        this.fk_cul_usuarios_id = fk_cul_usuarios_id;
-        this.fk_cul_posts_id = fk_cul_posts_id;
-        // this.data_criacao = data_criacao;
-        // this.data_mudanca = data_mudanca;
-
+    public Date getData_desativacao() {
+        return data_desativacao;
     }
 
-    public Curtida(long pk_id, long fk_cul_usuarios_id, long fk_cul_posts_id, Date data_criacao, Date data_mudanca) {
+    public void setData_desativacao(Date data_desativacao) {
+        this.data_desativacao = data_desativacao;
+    }
+
+    public Curtida(long fk_cul_usuarios_id, long fkCulPostsId, Date dataCriacao, Date data_mudanca, Date data_desativacao) {
+        this.fk_cul_usuarios_id = fk_cul_usuarios_id;
+        this.fkCulPostsId = fkCulPostsId;
+         this.dataCriacao = dataCriacao;
+         this.data_mudanca = data_mudanca;
+        this.data_desativacao = data_desativacao;
+    }
+
+    public Curtida(long pk_id, long fk_cul_usuarios_id, long fkCulPostsId, Date dataCriacao, Date data_mudanca, Date data_desativacao) {
         this.pk_id = pk_id;
         this.fk_cul_usuarios_id = fk_cul_usuarios_id;
-        this.fk_cul_posts_id = fk_cul_posts_id;
-        // this.data_criacao = data_criacao;
-        // this.data_mudanca = data_mudanca;
-
+        this.fkCulPostsId = fkCulPostsId;
+         this.dataCriacao = dataCriacao;
+         this.data_mudanca = data_mudanca;
+        this.data_desativacao = data_desativacao;
     }
 
     public long getPk_id() {
@@ -56,37 +67,38 @@ public class Curtida {
     }
 
     public long getFk_cul_posts_id() {
-        return fk_cul_posts_id;
+        return fkCulPostsId;
     }
 
-    public void setFk_cul_posts_id(long fk_cul_posts_id) {
-        this.fk_cul_posts_id = fk_cul_posts_id;
+    public void setFk_cul_posts_id(long fkCulPostsId) {
+        this.fkCulPostsId = fkCulPostsId;
     }
 
-    // public Date getData_criacao() {
-    // return data_criacao;
-    // }
+     public Date getData_criacao() {
+     return dataCriacao;
+     }
 
-    // public void setData_criacao(Date data_criacao) {
-    // this.data_criacao = data_criacao;
-    // }
+     public void setData_criacao(Date dataCriacao) {
+     this.dataCriacao = dataCriacao;
+     }
 
-    // public Date getData_mudanca() {
-    // return data_mudanca;
-    // }
+     public Date getData_mudanca() {
+     return data_mudanca;
+     }
 
-    // public void setData_mudanca(Date data_mudanca) {
-    // this.data_mudanca = data_mudanca;
-    // }
+     public void setData_mudanca(Date data_mudanca) {
+     this.data_mudanca = data_mudanca;
+     }
 
     @Override
     public String toString() {
         return "Curtida{" +
                 "pk_id=" + pk_id +
                 ", fk_cul_usuarios_id=" + fk_cul_usuarios_id +
-                ", fk_cul_posts_id=" + fk_cul_posts_id +
-                // ", data_criacao=" + data_criacao +
-                // ", data_mudanca=" + data_mudanca +
+                ", fkCulPostsId=" + fkCulPostsId +
+                 ", dataCriacao=" + dataCriacao +
+                 ", data_mudanca=" + data_mudanca +
+                ", data_desativacao=" + data_desativacao +
                 '}';
     }
 }
